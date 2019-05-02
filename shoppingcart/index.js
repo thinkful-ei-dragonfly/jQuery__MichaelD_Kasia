9574637c5be86069ebb457e0709eb7b19d5a21ee
Kasia <kasia@miroartdesign.com>
2019-05-02 18:51:11 -0400

    final shoppingList

--- shoppingcart/index.js
+++ shoppingcart/index.js
@@ -1,35 +1,9 @@
 ('use strict');
 /*$*/
 
-
-function itemIntput(){
-    let item;
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-        item = $('shopping-list-entry').val();
-        $('shopping-list-entry').val('')
-        
-    })
-
-    const html = generateItem(item);
-    
-
-
-    
-}
-
-console.log(itemIntput());
-
-
-
-
-
-
-
-
 function generateItem(item) {
     return ` <li>
-    <span class="shopping-item shopping-item__checked">${item}</span>
+    <span class="shopping-item">${item}</span>
     <div class="shopping-item-controls">
       <button class="shopping-item-toggle">
         <span class="button-label">check</span>
@@ -39,5 +13,35 @@ function generateItem(item) {
       </button>
     </div>
   </li>`;
-  }
-  console.log(generateItem('apples'));
+}
+
+function itemChecked() {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+}
+
+function itemDeleted() {
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+      });
+}
+
+function itemInput(){
+    let item;
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        item = $('#shopping-list-entry').val();
+        $('#shopping-list-entry').val('');
+        const html = generateItem(item);
+        $('.shopping-list').append(html);
+    });
+}
+
+function bundleActions() {
+    itemInput(),
+    itemChecked(),
+    itemDeleted()
+}
+
+$(bundleActions());
