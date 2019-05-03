f4ce84a4f8e7889e3a97fea8278e692685eabbbb
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-02 23:02:45 -0400

    resloved conflicts

--- shoppingcart/index.js
+++ shoppingcart/index.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 ('use strict');
 /*$*/
-
-<<<<<<< HEAD
 
 function itemIntput() {
   let item;
@@ -40,11 +40,6 @@ function checkItem() {
 function generateItem(item) {
   return ` <li>
     <span class="shopping-item ">${item}</span>
-=======
-function generateItem(item) {
-    return ` <li>
-    <span class="shopping-item">${item}</span>
->>>>>>> 9574637c5be86069ebb457e0709eb7b19d5a21ee
     <div class="shopping-item-controls">
       <button class="shopping-item-toggle">
         <span class="button-label">check</span>
@@ -56,43 +51,8 @@ function generateItem(item) {
   </li>`;
 }
 
-<<<<<<< HEAD
 $(function() {
   itemIntput();
   deleteItem();
   checkItem();
-
-
 });
-=======
-function itemChecked() {
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
-}
-
-function itemDeleted() {
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
-      });
-}
-
-function itemInput(){
-    let item;
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-        item = $('#shopping-list-entry').val();
-        $('#shopping-list-entry').val('');
-        const html = generateItem(item);
-        $('.shopping-list').append(html);
-    });
-}
-
-function bundleActions() {
-    itemInput(),
-    itemChecked(),
-    itemDeleted()
-}
-
-$(bundleActions());
->>>>>>> 9574637c5be86069ebb457e0709eb7b19d5a21ee
